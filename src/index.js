15b4def32349f8672d1b2fd68195b6cfb4bd6273
Ram Gaikar <gaikar1990@gmail.com>
2022-03-15 22:32:19 +0530

    Changed Router to Hash Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
